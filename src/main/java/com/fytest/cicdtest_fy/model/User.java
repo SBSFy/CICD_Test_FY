2b456cfce821fe3d29939bd5337546e8b5cd17ce
Fengyuan1430 <ting@strongbytestudio.com>
2022-08-08 18:10:25 +0800

    Setup

---
+++ src/main/java/com/fytest/cicdtest_fy/model/User.java
@@ -0,0 +1,21 @@
+package com.fytest.cicdtest_fy.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "test_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String password;
+}
