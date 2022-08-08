fb73cb5ad2717a2d99e559649fa5d715198c50f1
Fengyuan1430 <ting@strongbytestudio.com>
2022-08-08 17:50:47 +0800

    First Commit (Setup)

---
+++ src/main/java/com/fytest/cicdtest_fy/CicdTestFyApplication.java
@@ -0,0 +1,13 @@
+package com.fytest.cicdtest_fy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestFyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdTestFyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fytest/cicdtest_fy/CicdTestFyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fytest.cicdtest_fy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestFyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
