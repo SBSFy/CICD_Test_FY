bace1e27a90d1eaa87cd26694a9a9753ab595020
Fengyuan1430 <ting@strongbytestudio.com>
2022-08-09 14:35:49 +0800

    Disable db config & test case

--- src/main/java/com/fytest/cicdtest_fy/CicdTestFyApplication.java
+++ src/main/java/com/fytest/cicdtest_fy/CicdTestFyApplication.java
@@ -1,9 +1,17 @@
 package com.fytest.cicdtest_fy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+@Component
+@ComponentScan
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class CicdTestFyApplication {
 
     public static void main(String[] args) {
